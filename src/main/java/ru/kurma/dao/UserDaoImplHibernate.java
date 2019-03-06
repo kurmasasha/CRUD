ebb3ca44224670e0927a8619da912f9cac6a661c
sankur mac os <kurmasasha@gmail.com>
2019-03-06 12:29:14 +0300

    Session Factory

--- src/main/java/ru/kurma/dao/UserDaoImplHibernate.java
+++ src/main/java/ru/kurma/dao/UserDaoImplHibernate.java
@@ -1,11 +1,15 @@
 package ru.kurma.dao;
 
+import org.hibernate.SessionFactory;
 import ru.kurma.model.User;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoImplHibernate implements UserDao {
+
+    private SessionFactory sessionFactory;
+
     @Override
     public List<User> findAllUsers() throws SQLException {
         return null;
